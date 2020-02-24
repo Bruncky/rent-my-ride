aa7ffc14e210c947e23b6e43c97ebd56e1aa2aa0
Tim Vierboom <tim-vierboom@gmx.de>
2020-02-24 17:53:10 +0100

    Created models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many_attached :image
-  validates :name, presence: true
+  validates :model, presence: true
   validates :location, presence: true
   validates :price, presence: true
   validates :description, presence: true
