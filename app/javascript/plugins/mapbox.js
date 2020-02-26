ff5551c2d0c6af1e06ae07a253d662a2ceb96dc3
Bruncky <brunobrunckhorst@gmail.com>
2020-02-26 12:57:56 +0100

    Linked JS files

--- app/javascript/plugins/mapbox.js
+++ app/javascript/plugins/mapbox.js
@@ -1,47 +1,47 @@
-// Comment!
-import mapboxgl from '../node_modules/mapbox-gl';
+// // Comment!
+// import mapboxgl from '../node_modules/mapbox-gl';
 
-const inputField = document.getElementById("location-input");
-const submitButton = document.getElementById("submit-button");
-const form = document.querySelector("form");
+// const inputField = document.getElementById("location-input");
+// const submitButton = document.getElementById("submit-button");
+// const form = document.querySelector("form");
 
-const baseURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
-const apiToken = "pk.eyJ1IjoiYnJ1bmNreSIsImEiOiJjazZjM3FtNHAxNGU0M2RxanNtZTg5bGN6In0.gH3dsp-WaeDe_iU2Rd-XzA";
+// const baseURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+// const apiToken = "pk.eyJ1IjoiYnJ1bmNreSIsImEiOiJjazZjM3FtNHAxNGU0M2RxanNtZTg5bGN6In0.gH3dsp-WaeDe_iU2Rd-XzA";
 
-// -------------------- GET --------------------
+// // -------------------- GET --------------------
 
-const fetchMap = (longitude, latitude) => {
-  mapboxgl.accessToken = apiToken;
+// const fetchMap = (longitude, latitude) => {
+//   mapboxgl.accessToken = apiToken;
 
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [longitude, latitude],
-    zoom: 12
-  });
+//   const map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9',
+//     center: [longitude, latitude],
+//     zoom: 12
+//   });
 
-  const marker = new mapboxgl.Marker()
-    .setLngLat([longitude, latitude])
-    .addTo(map);
-};
+//   const marker = new mapboxgl.Marker()
+//     .setLngLat([longitude, latitude])
+//     .addTo(map);
+// };
 
-const fetchCoordinates = (city) => {
-  fetch(`${baseURL}${city}.json?access_token=${apiToken}`)
-    .then(response => response.json())
-    .then((data) => {
-      const longitude = data.features[0].geometry.coordinates[0];
-      const latitude = data.features[0].geometry.coordinates[1];
+// const fetchCoordinates = (city) => {
+//   fetch(`${baseURL}${city}.json?access_token=${apiToken}`)
+//     .then(response => response.json())
+//     .then((data) => {
+//       const longitude = data.features[0].geometry.coordinates[0];
+//       const latitude = data.features[0].geometry.coordinates[1];
 
-      document.querySelector(".coordinates").innerText = `${longitude} | ${latitude}`;
+//       document.querySelector(".coordinates").innerText = `${longitude} | ${latitude}`;
 
-      fetchMap(longitude, latitude);
-    });
-};
+//       fetchMap(longitude, latitude);
+//     });
+// };
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
 
-  fetchCoordinates(inputField.value);
-});
+//   fetchCoordinates(inputField.value);
+// });
 
-fetchCoordinates("São Paulo");
+// fetchCoordinates("São Paulo");
