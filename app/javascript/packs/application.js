8856e1abaa91732fcc6258064957f9165c25f9b6
Tim Vierboom <tim-vierboom@gmx.de>
2020-02-26 16:22:30 +0100

    Added simple confirmation alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,3 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
-
-$(".changeit").on("submit", function(){
-    setTimeout(function() {
-      location.href='http://localhost:3000/users/12';
-    }, 10)
-  }
