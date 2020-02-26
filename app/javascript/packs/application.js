1b2a2eb5f35e2760d3c10be930f9b07d0cf901cc
Tim Vierboom <tim-vierboom@gmx.de>
2020-02-26 14:32:42 +0100

    bla

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,9 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+
+$(".changeit").on("submit", function(){
+    setTimeout(function() {
+      location.href='http://localhost:3000/users/12';
+    }, 10)
+  }
+
