3a2784414eda5418dd235e228f84905eb12fa1bc
Finn Rub <r.finn40@yahoo.com>
2020-02-26 21:18:58 +0100

    change query string to ?query=

--- app/javascript/plugins/search.js
+++ app/javascript/plugins/search.js
@@ -3,5 +3,5 @@ const submit = document.querySelector('#submit-button')
 submit.addEventListener('click', function (event) {
   event.preventDefault();
   const location = document.querySelector('#location-input').value
-  window.open(`/cars?=query${location}`,"_self")
+  window.open(`/cars?query=${location}`,"_self")
 });
