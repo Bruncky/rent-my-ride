dbfbafebe46e1fc1cec3e9ca56143832184442f3
Bruncky <brunobrunckhorst@gmail.com>
2020-02-26 11:52:30 +0100

    Typo fix

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   geocoded_by :full_address
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_full_address?
 
   belongs_to :user
   has_many :bookings, dependent: :destroy
