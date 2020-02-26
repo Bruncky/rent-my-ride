6b5ecb3d123ff9e3e09306b5f79360e6a57172bd
Finn Rub <r.finn40@yahoo.com>
2020-02-26 16:17:46 +0100

    search js function added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import "../plugins/flatpickr"
 import "../plugins/mapbox"
 import 'mapbox-gl/dist/mapbox-gl.css';
+import '../plugins/search';
 
 import { initMapbox } from '../plugins/init_mapbox';
 
