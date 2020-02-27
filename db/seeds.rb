8d40972b6b35bde7aca191f79ceb2bf68eb85d45
Bruncky <brunobrunckhorst@gmail.com>
2020-02-27 15:32:45 +0100

    Fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,13 @@ pics = [
   "https://res.cloudinary.com/dbqegklxb/image/upload/v1582716928/dmBRo9qJe2ZbeCQ2WTnVNFd2.jpg",
 ]
 
+gallery = [
+  "https://res.cloudinary.com/dbqegklxb/image/upload/v1582716953/7CfzycbUaRbfLWEaUxef5gtj.jpg",
+  "https://res.cloudinary.com/dbqegklxb/image/upload/v1582716950/phu6H9bDRdbAhmXPFgnCbBTY.jpg",
+  "https://res.cloudinary.com/dbqegklxb/image/upload/v1582716947/fA9zj1HFyboq9iDjwM1FTnec.jpg",
+  "https://res.cloudinary.com/dbqegklxb/image/upload/v1582716944/ZiCs4sWsadmCn8F89kAxZAkW.jpg"
+  ]
+
 addresses = [
   "Hafenplatz 5 Berlin",
   "Moderssohnstrasse 60 Berlin",
@@ -52,9 +59,9 @@ i = 0
   car.user = user
 
   file = URI.open(pics[i])
-  car.thumbnail.attach(io: file, filename: 'car.jpeg', content_type: 'image/jpg')
 
-  car.photos.attach(io: pics)
+  car.thumbnail.attach(io: file, filename: 'car.jpeg', content_type: 'image/jpg')
+  gallery.each { |photo| car.photos.attach(io: URI.open(photo), filename: 'car.jpeg') }
 
   car.save!
 
