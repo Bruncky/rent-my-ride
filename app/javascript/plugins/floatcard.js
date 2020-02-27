30cacd38c2af0029ef34ffe64999020f1df96299
Patrick Plue <pluepatrick@gmail.com>
2020-02-27 18:15:17 +0100

    added modal for car index

--- app/javascript/plugins/floatcard.js
+++ app/javascript/plugins/floatcard.js
@@ -2,11 +2,15 @@ const cardimg = document.querySelectorAll('.card');
 const background = document.querySelector('.cardbig');
 
 cardimg.forEach((element) => {
-element.addEventListener('click', (event) => {  const key = event.currentTarget.dataset.carId
-const hiddencard = document.querySelector(`[data-car-hidden-id='${key}']`);
-hiddencard.classList.toggle("hidden");
-hiddencard.classList.toggle("floatcard");
-background.classList.toggle("hidden");
+  element.addEventListener('click', (event) => {  const key = event.currentTarget.dataset.carId
+  const hiddencard = document.querySelector(`[data-car-hidden-id='${key}']`);
+  const closecard = document.querySelector(`[data-close-hidden-id='${key}']`);
+    hiddencard.classList.toggle("hidden");
+    hiddencard.classList.toggle("floatcard");
+    background.classList.toggle("hidden");
+  closecard.addEventListener('click', (event) => {
+    hiddencard.classList.add("hidden");
+    background.classList.toggle("hidden");
+  });
  });
 });
-
