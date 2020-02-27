6fedb97f2574667c5e8e06880a6e76738fef925c
Patrick Plue <pluepatrick@gmail.com>
2020-02-27 17:38:46 +0100

    changed the blur animation of home

--- app/javascript/plugins/backgroundblur.js
+++ app/javascript/plugins/backgroundblur.js
@@ -2,6 +2,9 @@ const form = document.querySelector('form');
 
 const bg = document.querySelector('.backgroundhome');
 
+const tagLine = document.querySelector('h2');
+
 form.addEventListener('click', (event) => {
-  bg.classList.toggle('blur')
+  bg.classList.toggle('blur');
+  tagLine.classList.toggle('blur');
 });
