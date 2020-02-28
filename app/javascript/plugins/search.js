5b9693ab6fc6e598ebc078b19d8e474254a34eb2
Finn Rub <r.finn40@yahoo.com>
2020-02-28 08:50:38 +0100

    search 2.0 is now working with date filters

--- app/javascript/plugins/search.js
+++ app/javascript/plugins/search.js
@@ -4,5 +4,9 @@ submit.addEventListener('click', (event) => {
   event.preventDefault();
 
   const location = document.querySelector('#location-input').value
-  window.open(`/cars?query=${location}`,"_self")
-});
+  const startDate = document.querySelector('#start_date').value
+  const endDate = document.querySelector('#end_date').value
+  window.open(`/cars?query=${location}&amp;start_date=${startDate}&amp;end_date=${endDate}`,"_self")
+
+
+// window.open(`/cars?query=${location}&start_date=${startDate}&end_date=${endDate}`,"_self")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_101538) do
+ActiveRecord::Schema.define(version: 2020_02_27_140429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,13 +37,13 @@ ActiveRecord::Schema.define(version: 2020_02_27_101538) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.date "start_date"
+    t.datetime "start_date"
     t.bigint "user_id"
     t.bigint "car_id"
     t.boolean "confirmed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "end_date"
+    t.datetime "end_date"
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
