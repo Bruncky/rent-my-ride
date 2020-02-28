aacae8830bdecfad0b86a7c6a4105bcdb18709aa
Tim Vierboom <tim-vierboom@gmx.de>
2020-02-28 12:14:30 +0100

    Implement a simple but nicer dateppicker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,14 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
-flatpickr(".datepicker", {})
+// flatpickr(".datepicker", {
+//   rangePlugin: {"plugins": [new rangePlugin({ input: "#secondRangeInput"})]}
+//   // minDate: "today",
+//   // dateFormat: "Y-m-d",
+// })
+
+flatpickr(".datepicker", {
+    "plugins": [new rangePlugin({ input: ".secondRangeInput"})]
+});
+
+
