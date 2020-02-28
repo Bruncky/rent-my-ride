126d37466a16482b4c678fb0ea5cdafd62e7d308
Bruncky <brunobrunckhorst@gmail.com>
2020-02-28 23:39:22 +0100

    Commented mapbox CSS back into code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 // import "../plugins/sweetalert"
 // import "../plugins/mapbox"
-// import 'mapbox-gl/dist/mapbox-gl.css';
 // import '../plugins/search';
 
 import { initMapbox } from '../plugins/init_mapbox';
