5eaf3ce5d15232ba5ead60b771caa5899f536a41
Patrick Plue <pluepatrick@gmail.com>
2020-02-28 11:05:31 +0100

    fixed blur issue and made datepicker the same color

--- app/javascript/plugins/backgroundblur.js
+++ app/javascript/plugins/backgroundblur.js
@@ -5,6 +5,6 @@ const bg = document.querySelector('.backgroundhome');
 const tagLine = document.querySelector('h2');
 
 form.addEventListener('click', (event) => {
-  bg.classList.toggle('blur');
-  tagLine.classList.toggle('blur');
+  bg.classList.add('blur');
+  tagLine.classList.add('blur');
 });
